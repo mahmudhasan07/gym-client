39d1e1570e283f2e1355ccb4abca0a78edf8cc76
unknown <mahmudhasan.hb@gmail.com>
2024-10-11 23:50:47 +0600

    complete readme file

--- src/app/admin/Admin.jsx
+++ src/app/admin/Admin.jsx
@@ -1,23 +1,30 @@
 'use client'
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ContextSource } from '../components/ContextAPI/ContextAPI';
+import useFetch2 from '../components/Hooks/useFetch2';
+import Modal from '../components/Modal/Modal';
 
 const Admin = () => {
 
     const { user } = useContext(ContextSource)
+    const [data, refetch] = useFetch2('user', user?.email)
+    const [modal, setModal] = useState(false)
     return (
         <section className='bg-gray-300 lg:w-4/5 md:w-3/4 w-full lg:h-auto md:h-auto h-screen  relative '>
             <div className='border-2 lg:w-1/2 md:w-4/5 bg-white rounded-lg relative top-[15%] mx-auto p-5 border-gray-500'>
-                <img src={user?.photo} className='w-80 object-cover h-80 mx-auto rounded-full' alt="" />
-                <button className='text-blue-700 font-semibold text-lg text-end w-full'>Edit</button>
+                <img src={data?.photo} className='w-80 object-cover h-80 mx-auto rounded-full' alt="" />
+                <button onClick={()=> setModal(true)} className='text-blue-700 font-semibold text-lg text-end w-full'>Edit</button>
+                <dialog open={modal}>
+                    <Modal name={data?.name} email={data?.email} photo={data?.photo} setModal={setModal} refetch={refetch}></Modal>
+                </dialog>
                 <div className='text-xl'>
                     <div>
                         <label htmlFor="" className='font-bold'>Name:</label>
-                        <h1>{user?.name}</h1>
+                        <h1>{data?.name}</h1>
                     </div>
                     <div>
                         <label htmlFor="" className='font-bold'>Email:</label>
-                        <h1>{user?.email}</h1>
+                        <h1>{data?.email}</h1>
                     </div>
                 </div>
             </div>
